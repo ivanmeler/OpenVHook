f4d59f4af74c616014da37d5bff281551c545a30
Anthony Titus <32780683+212antt@users.noreply.github.com>
2020-05-21 19:29:46 -0400

    Possible fix for broken Tread Pools

--- OpenVHook/Scripting/Pools.cpp
+++ OpenVHook/Scripting/Pools.cpp
@@ -11,7 +11,7 @@ bool CPools::Initialize()
 	executable_meta executable;
 	executable.EnsureInit();
 
-	auto pedPoolPattern = pattern("48 8B 05 ? ? ? ? 48 63 50 10");
+	auto pedPoolPattern = pattern("48 8B 05 ? ? ? ? 41 0F BF C8 0F BF 40 10"); 
 
 	char * location = pedPoolPattern.count(1).get(0).get<char>(3);
 	if (location == nullptr) {
@@ -22,7 +22,7 @@ bool CPools::Initialize()
 	m_pPedPool = reinterpret_cast<decltype(m_pPedPool)>(location + *(int32_t*)location + 4);
 	LOG_DEBUG("pedPool\t 0x%p (0x%.8X)", m_pPedPool, reinterpret_cast<uintptr_t>(m_pPedPool) - executable.begin());
 
-	auto objectPoolPattern = pattern("4C 8B 15 ? ? ? ? 85 ED");
+	auto objectPoolPattern = pattern("48 8B 05 ? ? ? ? 8B 78 10 85 FF"); 
 
 	location = objectPoolPattern.count(1).get(0).get<char>(3);
 	if (location == nullptr) {
@@ -33,7 +33,7 @@ bool CPools::Initialize()
 	m_pObjectPool = reinterpret_cast<decltype(m_pObjectPool)>(location + *(int32_t*)location + 4);
 	LOG_DEBUG("objectPool\t 0x%p (0x%.8X)", m_pObjectPool, reinterpret_cast<uintptr_t>(m_pObjectPool) - executable.begin());
 
-	auto pickupPoolPattern = pattern("4C 8B 05 ? ? ? ? 41 8B 58 20");
+	auto pickupPoolPattern = pattern("4C 8B 05 ? ? ? ? 40 8A F2 8B E9");  
 
 	location = pickupPoolPattern.count(1).get(0).get<char>(3);
 	if (location == nullptr) {
@@ -44,7 +44,7 @@ bool CPools::Initialize()
 	m_pPickupsPool = reinterpret_cast<decltype(m_pPickupsPool)>(location + *(int32_t*)location + 4);
 	LOG_DEBUG("pickupsPool\t 0x%p (0x%.8X)", m_pPickupsPool, reinterpret_cast<uintptr_t>(m_pPickupsPool) - executable.begin());
 
-	auto vehiclePoolPattern = pattern("4C 8B 3D ? ? ? ? 4D 8B 0F");
+	auto vehiclePoolPattern = pattern("48 8B 05 ? ? ? ? F3 0F 59 F6 48 8B 08"); 
 
 	location = vehiclePoolPattern.count(1).get(0).get<char>(3);
 	if (location == nullptr) {
@@ -55,7 +55,7 @@ bool CPools::Initialize()
 	m_pVehiclePool = reinterpret_cast<decltype(m_pVehiclePool)>(location + *(int32_t*)location + 4);
 	LOG_DEBUG("vehiclePool\t 0x%p (0x%.8X)", m_pVehiclePool, reinterpret_cast<uintptr_t>(m_pVehiclePool) - executable.begin());
 
-	auto entityPoolPattern = pattern("4C 8B 05 ? ? ? ? 49 2B 00");
+	auto entityPoolPattern = pattern("4C 8B 0D ? ? ? ? 44 8B C1 49 8B 41 08");  
 
 	location = entityPoolPattern.count(1).get(0).get<char>(3);
 	if (location == nullptr) {
